7251fd381bd166ca0d294aa79dca110e8df6aafe
david fletcher <davidfletcheraaron@gmail.com>
2022-01-10 13:35:39 -0500

    hello test is running thru

--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+// const Users =require('./users/model')
+// const usersRouter = require('./users/usersRouter')
 
 const server = express();
 
+server.use(express.json())
 
-module.exports = { server }; // EXPORT YOUR SERVER instead of {}
+// server.use('/api/users', usersRouter)
+
+server.get('/hello', (req, res)=>{
+   res.json({ message: 'hi im here'})
+})
+
+server.get('/api/users', (req, res)=>{
+
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const server = require('./api/server');
 
 const port = 9000;
 
+
+
 // START YOUR SERVER HERE
-server.listen(port, ()=>{
+server.listen(port, (req, res)=>{
    console.log(`server running on port ${port}`)
 }) 
