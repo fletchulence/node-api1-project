a1ca8de5853ae0cd0295823a0d472894650d0109
david fletcher <davidfletcheraaron@gmail.com>
2022-01-10 14:29:12 -0500

    users by id is working

--- api/server.js
+++ api/server.js
@@ -40,13 +40,14 @@ server.post('/api/users', async (req, res)=>{
 
 // [GET] getById
 server.get('/api/users/:id', async (req, res)=>{
-   const { id } = req.params.id
-   const user = await User.findById( id )
+   const { id }  = req.params
+   const user = await User.findById(id)
    try{
       if( !user ){
-         res.status(400)
+         console.log(id)
+         res.status(404).json({ message: 'The user with the specified ID does not exist'})
       } else{
-
+         res.status(200).json(user)
       }
    } catch(err){
       res.status(500).json({ message: err.message })
